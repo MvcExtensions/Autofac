c050f1975b9413bcc003bc38686d35569a1f8df3
Alexander Zaytsev <hazzik@gmail.com>
2011-10-29 22:01:51 +0600

    Removed useless ExtendedDependencyResolver

--- src/MvcExtensions.Autofac/AutofacAdapter.cs
+++ src/MvcExtensions.Autofac/AutofacAdapter.cs
@@ -125,7 +125,7 @@ namespace MvcExtensions.Autofac
         /// </summary>
         /// <param name="serviceType">Type of the service.</param>
         /// <returns></returns>
-        protected override object DoGetService(Type serviceType)
+        public override object GetService(Type serviceType)
         {
             return RequestLifetimeScope.ResolveOptional(serviceType);
         }
@@ -135,7 +135,7 @@ namespace MvcExtensions.Autofac
         /// </summary>
         /// <param name="serviceType">Type of the service.</param>
         /// <returns></returns>
-        protected override IEnumerable<object> DoGetServices(Type serviceType)
+        public override IEnumerable<object> GetServices(Type serviceType)
         {
             Type type = typeof(IEnumerable<>).MakeGenericType(serviceType);
 
